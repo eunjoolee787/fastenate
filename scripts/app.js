1ac9a6d7bdbd6264d721fd99490d19e87b851c50
Eun Joo <eunjoolee787@gmail.com>
2014-11-19 22:05:29 -1000

    created for loops

--- scripts/app.js
+++ scripts/app.js
@@ -1,20 +1,48 @@
 $(function(){
-  function viewBlogPosts(){};
-  function viewStudents(){};
-$(".one").on("click", function(){});
-$(".two").on("click", function(){});
-  $.ajax({
-  type: GET,
-  url: https://api.mongolab.com/api/1/databases/devleague_jquery/collections/blog_posts
-  contentType: "application/json",
-  data: {
-    https://api.mongolab.com/api/1/databases/devleague_jquery/collections/students
-  },
-  success: function(response){
-    console.log(response);
-  },
-  error: function(request, errorType, errorMessage){
-    console.log("ERROR: " + errorMessage);
+  function viewBlogPosts(){
+
   }
-});
+  function viewStudents(){
+
+  }
+  $(".blogs").on("click", function(){
+    $.ajax({
+      type: "GET",
+      url: "https://api.mongolab.com/api/1/databases/devleague_jquery/collections/blog_posts",
+      contentType: "application/json",
+      data: {
+        "apiKey": "QMnw2qjgl4l0LDx3RbN2NRlMEu_vCipC"
+      },
+      success: function(response){
+        console.log(response);
+        for(var i = 0; i < response.length; i++){
+          console.log(response[i]);
+         var blogContainer = $("<div>");
+         // EJL-finish tomorrow
+        }
+      },
+      error: function(request, errorType, errorMessage){
+        console.log("ERROR: " + errorMessage);
+      }
+    });//end ajax
+  });
+
+  $(".students").on("click", function(){
+    $.ajax({
+      type: "GET",
+      url:  "https://api.mongolab.com/api/1/databases/devleague_jquery/collections/students",
+      contentType: "application/json",
+      data: {
+        "apiKey": "QMnw2qjgl4l0LDx3RbN2NRlMEu_vCipC"
+      },
+      success: function(response){
+        console.log(response);
+      },
+      error: function(request, errorType, errorMessage){
+        console.log("ERROR: " + errorMessage);
+      }
+    });//end ajax
+  });
+
+});//document ready
 
