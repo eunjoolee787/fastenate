a539a336950fd1132edfbe79871dc2145646796b
Eun Joo <eunjoolee787@gmail.com>
2014-11-20 22:01:37 -1000

    added content

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,7 @@
 $(function(){
-  function viewBlogPosts(){
+  function viewBlogPosts(event){
 
-  }
-  function viewStudents(){
-
-  }
-  $(".blogs").on("click", function(){
+    event.preventDefault();
     $.ajax({
       type: "GET",
       url: "https://api.mongolab.com/api/1/databases/devleague_jquery/collections/blog_posts",
@@ -14,18 +10,38 @@ $(function(){
         "apiKey": "QMnw2qjgl4l0LDx3RbN2NRlMEu_vCipC"
       },
       success: function(response){
-        console.log(response);
+        //response Array of Mongo Documents(json objects)
+        //console.log(response);
         for(var i = 0; i < response.length; i++){
-          console.log(response[i]);
-         var blogContainer = $("<div>");
-         // EJL-finish tomorrow
-        }
+          var json_doc = response[i];
+          //console.log(json_doc);
+
+          var body_content = $("<div>");
+          body_content.addClass("body_content");
+
+          var body_list = $("<div>");
+          body_list.addClass("body_list");
+          body_content.append(body_list);
+
+          var author_p_element = $("<p>");
+          author_p_element.addClass("author");
+          author_p_element.html(json_doc.author);
+          body_container.append(author_p_element);
+
+          $("#body_container").append(body_content);
+        };
       },
-      error: function(request, errorType, errorMessage){
+        error: function(request, errorType, errorMessage){
         console.log("ERROR: " + errorMessage);
-      }
+        }
     });//end ajax
-  });
+  }
+
+
+  function viewStudents(){
+
+  }
+  $("li.blogs").on("click", viewBlogPosts);
 
   $(".students").on("click", function(){
     $.ajax({
